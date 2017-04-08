fa36ec927ddf5f9e91163aab2b344bec06335dcf
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-09 01:01:03 +0700

    Call student seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
         $this->call('UserSeeder');
         $this->call('MovieSeeder');
+        $this->call('StudentSeeder');
     }
 }
