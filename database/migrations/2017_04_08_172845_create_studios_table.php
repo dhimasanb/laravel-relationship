6c40f26a5af1c4be2755917cdd124d51f5676cb0
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-09 00:33:45 +0700

    Create studios migrations

---
+++ database/migrations/2017_04_08_172845_create_studios_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudiosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('studios', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->date('founded_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('studios');
+    }
+}
