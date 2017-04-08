3babe7e7386bda4d74c9fc390569fc42ab9093a5
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-09 00:33:34 +0700

    Create movies migrations

---
+++ database/migrations/2017_04_08_172924_create_movies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->date('date_released');
+            $table->integer('studio_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
