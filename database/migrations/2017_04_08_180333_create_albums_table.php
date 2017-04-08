0daa47d44ffb20a126f7f1fdd328011c50724ae5
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-09 01:29:41 +0700

    Create album migration

---
+++ database/migrations/2017_04_08_180333_create_albums_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAlbumsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('albums', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('artist_id')->unsigned();
+			      $table->string('title');
+			      $table->date('released');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('albums');
+    }
+}
