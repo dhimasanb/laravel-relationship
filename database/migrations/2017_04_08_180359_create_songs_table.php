2ddbb2bc3c20cd476737c0f0455a1458c0d83e41
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-09 01:29:51 +0700

    Create songs migration

---
+++ database/migrations/2017_04_08_180359_create_songs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSongsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('songs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('album_id')->unsigned();
+			      $table->string('title');
+			      $table->time('length');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('songs');
+    }
+}
