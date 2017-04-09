901d6187eee26682ba6aac03f276a5b703698160
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-09 20:08:59 +0700

    Call tag seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -18,5 +18,6 @@ class DatabaseSeeder extends Seeder
         $this->call('SongSeeder');
         $this->call('CommentSeeder');
         $this->call('PersonSeeder');
+        $this->call('TagSeeder');
     }
 }
