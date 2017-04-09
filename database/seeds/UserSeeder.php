fdfb3a4bb56ad66350a1fd6ad7706809587d19e3
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-09 21:01:19 +0700

    Change user id to account id

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -15,9 +15,28 @@ class UserSeeder extends Seeder
         App\User::create(['name'=>'Dani', 'email'=>'dani@gmail.com', 'password'=>Hash::make('passW0rd')]);
         App\User::create(['name'=>'Deon', 'email'=>'deon@gmail.com', 'password'=>Hash::make('secret')]);
 
-        App\Preference::create(['user_id'=>1, 'country'=>'ID', 'currency'=>'IDR', 'subscribe_mailing_list'=>1]);
-        App\Preference::create(['user_id'=>2, 'country'=>'MY', 'currency'=>'MYR', 'subscribe_mailing_list'=>0]);
-        App\Preference::create(['user_id'=>3, 'country'=>'SG', 'currency'=>'SGD', 'subscribe_mailing_list'=>1]);
+        // App\Preference::create(['user_id'=>1, 'country'=>'ID', 'currency'=>'IDR', 'subscribe_mailing_list'=>1]);
+        // App\Preference::create(['user_id'=>2, 'country'=>'MY', 'currency'=>'MYR', 'subscribe_mailing_list'=>0]);
+        // App\Preference::create(['user_id'=>3, 'country'=>'SG', 'currency'=>'SGD', 'subscribe_mailing_list'=>1]);
+
+        App\User::find(1)->preference()->save(App\Preference::create([
+            'account_id'             => 1,
+            'country'                => 'ID',
+            'currency'               => 'IDR',
+            'subscribe_mailing_list' => 1])
+        );
+        App\User::find(2)->preference()->save(App\Preference::create([
+            'account_id'             => 2,
+            'country'                => 'MY',
+            'currency'               => 'MYR',
+            'subscribe_mailing_list' => 0])
+        );
+        App\User::find(2)->preference()->save(App\Preference::create([
+            'account_id'             => 3,
+            'country'                => 'SG',
+            'currency'               => 'SGD',
+            'subscribe_mailing_list' => 1])
+        );
 
     }
 }
